9278a4b4deeb9eb34fa8de266194267d9247906e
eissah <naseissah12@gmail.com>
2019-06-25 15:37:13 +0300

    new branch

--- trucks/forms.py
+++ trucks/forms.py
@@ -3,7 +3,7 @@ from .models import Trucks
 
 class NewTruckForm(forms.ModelForm):
     class Meta:
-        model: Trucks
+        model: 'Trucks'
         fields: ['entry_no','truck_no','conatiner','client']
         labels:{'entry_no':'Entry Number','truck_no':'Truck','Container':'Container','client':'Client'}
     
