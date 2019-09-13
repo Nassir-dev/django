ef1ef8cdcd514c073204de3fa37025dc8a1d43f8
Issa <knholdings@yahoo.com>
2019-09-13 12:32:20 +0300

    adjusted the count

--- trucks/views.py
+++ trucks/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse,HttpResponseRedirect
 from django.urls import reverse
 from .models import Trucks
+from client_app.models import Client
 
 
 
@@ -11,8 +12,13 @@ from .models import Trucks
 # Create your views here.
 def homepage(request):
     trucks = Trucks.objects.order_by('date_handled')
-
-    context = {'trucks':trucks}
+    ashiley = Trucks.objects.filter(client__name__iexact='ashiley').count()
+    lutor = Trucks.objects.filter(client__name__iexact='lutor').count()
+    daneva = Trucks.objects.filter(client__name__iexact='daneva').count()
+    context = {'trucks':trucks, 
+                'ashiley':ashiley,
+                'lutor':lutor,
+                'daneva':daneva}
     return render(request, 'trucks/index.html',context)
 
 def aboutpage(request):
